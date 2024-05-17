34ae6eee117de4f9e10141fdb996f96d3ebe78f1
Sneyder M <sneider.caicedo@gmail.com>
2024-05-16 20:23:57 -0500

    Llamado de api con params, body y headers.

--- sendEmail.py
+++ sendEmail.py
@@ -1,15 +1,27 @@
 import requests
+from dotenv import load_dotenv
+import os
 
 def call_api():
-    url = "https://api-colombia.com/api/v1/Department"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
+    load_dotenv()
 
-        data = response.json()
-        print('Información de los departamentos de Colombia: \n {}'.format(data))
-        
+    url = "https://wigaapimanagement.azure-api.net/deploymentEmail/correo"
+    payload = { "despliegue": "Wigaming" }
+    headers = {
+        "Ocp-Apim-Subscription-Key": os.getenv("SUBSCRIPTION_KEY"),
+        "id": os.getenv("ID"),
+    }
+    body = {
+        "asunto": "Actualización Wigaming since SCRIPT EN PYTHON",
+        "destinatarios": [ "sneider.caicedo@gmail.com" ],
+        "img": "https://wiscanstorage.blob.core.windows.net/pantillas-despliegues/wigaming.png"
+    }
+
+    try:
+        response = requests.post(url, params=payload, headers=headers, json=body)
+        print("Status Code {} --> Successfully {} ".format(response.status_code, response.json()))
     except requests.exceptions.RequestException as e:
-        print("Error al llamar a la API:", e)
+        print("Status Code Error {}".format(response.status_code))
+        print("Error en el envio del correo: ", e)
 
 call_api()
