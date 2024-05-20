34566a99a69413f62bd3613d2b22d5d4c95d6a2b
Sneyder M <sneyder.martinez@wiga.io>
2024-05-20 13:07:22 -0500

    Llamado dinamico al API

--- sendEmail.py
+++ sendEmail.py
@@ -5,8 +5,7 @@ import os
 def call_api():
     load_dotenv()
 
-    url = "https://wigaapimanagement.azure-api.net/deploymentEmail/correo"
-    payload = { "despliegue": "Wigaming" }
+    url = "https://wigaapimanagement.azure-api.net/deploymentEmail?despliegue={}".format(os.getenv("DEPLOYMENT"))
     headers = {
         "Ocp-Apim-Subscription-Key": os.getenv("SUBSCRIPTION_KEY"),
         "id": os.getenv("ID"),
@@ -14,14 +13,13 @@ def call_api():
     body = {
         "asunto": "Actualización Wigaming since SCRIPT EN PYTHON",
         "destinatarios": [ "sneider.caicedo@gmail.com" ],
-        "img": "https://wiscanstorage.blob.core.windows.net/pantillas-despliegues/wigaming.png"
+        "backlog": "Seguimiento y ajustes Wigaming F2"
     }
 
     try:
-        response = requests.post(url, params=payload, headers=headers, json=body)
+        response = requests.post(url, headers=headers, json=body)
         print("Status Code {} --> Successfully {} ".format(response.status_code, response.json()))
     except requests.exceptions.RequestException as e:
-        print("Status Code Error {}".format(response.status_code))
-        print("Error en el envio del correo: ", e)
+        print("Error: {}".format(e))
 
 call_api()
